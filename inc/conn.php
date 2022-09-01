7239c31ee08e8a2f18220294f64b21fccb288781
Sanjeewa <isharasanjeewa894@gmail.com>
2022-09-01 16:38:00 +0530

    modified signup page and created database connection and created registration backend

---
+++ inc/conn.php
@@ -0,0 +1,27 @@
+<?php
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "myproject";
+
+$conn = mysqli_connect($host,$username,$password,$database);
+
+// if(!$conn){
+
+//     die("Database connection failed " . mysqli_error($conn));
+// }
+
+// else{
+//     echo "Database connection success!!";
+// }
+
+
+
+// if ($conn->connect_error){
+//     die("Database connection failed " . $conn->connect_error);
+// }else{
+//     echo "Database connection success!!";
+// }
+
+?>
--- signup.php
+++ signup.php
@@ -1,3 +1,74 @@
+<?php include_once('inc/conn.php')?>
+
+<?php //error_reporting (E_ALL ^ E_NOTICE);
+
+    if(isset($_POST['submit'])){
+        $firstname = "";
+        $lastname = "";
+        $email = "";
+        $password = "";
+    }
+
+    // $firstname = isset($firstname) ? $firstname : '';
+    // $lastname = isset($lastname) ? $lastname: '';
+    // $email = isset($email) ? $email : '';
+    // $password = isset($password) ? $password : '';
+
+    $firstname = input_varify($_POST['FirstName']);    
+    $lastname = input_varify($_POST['LastName']);
+    $email = input_varify($_POST['EMail']);
+    $password = input_varify($_POST['Password']);
+
+    $query = "INSERT INTO tbl_user(Fname,Lname,email,pwd,Reg_DT) VALUES ('{$firstname}','{$lastname}','{$email}','{$password}',NOW())";
+
+    if (mysqli_query($conn, $query)){
+        echo "User Registration success!!";
+    }else{
+        echo "User Registration failed" . mysqli_error($conn);
+    }
+
+    // $result = mysqli_query($conn , $query);
+
+    // if($result){
+    //     echo "User Registration success!!";
+    // }
+
+    // else{
+    //     echo mysqli_connect_error($conn);
+
+    // }
+
+
+    // echo $firstname;
+    // echo "<br>";
+    // echo $lastname;
+    // echo "<br>";
+    // echo $email;
+    // echo "<br>";
+    // echo $password;
+    // echo "<br>";
+
+    // echo 'First Name '  . $firstname.'<br>';
+    // echo 'Last Name '  . $lastname.'<br>';
+    // echo 'First Name '  . $firstname.'<br>';
+    // echo 'E Mail' . $email. '<br>';
+    // echo 'Password' . $password. '<br>';
+
+
+
+    function input_varify($data){
+        $data = trim($data);
+        $data = stripcslashes($data);
+        $data = htmlspecialchars($data);
+
+        return $data;
+
+
+    }
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,39 +92,39 @@
                         <div class="card-header" id="card-header">
                             <h4>Sign Up Form</h4>
                         </div>
-                    <form action="index.php">
+                    <form action="signup.php" method="POST" autocomplete="off">
                         <div class="card-body" id="card-body">
                             <div class="form-group">
                               <label for="">First Name</label>
-                              <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+                              <input type="text" name="FirstName" id="FirstName" class="form-control" placeholder="" aria-describedby="helpId">
                               <small id="helpId" class="text-muted">Enter Your First Name</small>
                             </div>
 
                         
                             <div class="form-group">
                               <label for="">Last Name</label>
-                              <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+                              <input type="text" name="LastName" id="LastName" class="form-control" placeholder="" aria-describedby="helpId">
                               <small id="helpId" class="text-muted">Enter Your Last Name</small>
                             </div>
                             
                         
                             <div class="form-group">
                               <label for="">E Mail</label>
-                              <input type="email" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+                              <input type="email" name="EMail" id="EMail" class="form-control" placeholder="" aria-describedby="helpId">
                               <small id="helpId" class="text-muted">Enter Your E Mail</small>
                             </div>
                             
                         
                             <div class="form-group">
                               <label for="">Password</label>
-                              <input type="password" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+                              <input type="password" name="Password" id="Password" class="form-control" placeholder="" aria-describedby="helpId">
                               <small id="helpId" class="text-muted">Create Your Own Password</small>
                             </div>
 
                         </div>
 
                             <div class="card-footer" id="card-footer">
-                                <button type="submit" class="btn btn-primary">Sign Up</button>
+                                <button type="submit" name="submit" class="btn btn-primary">Sign Up</button>
                         </div>
                         </form>
                     </div>
